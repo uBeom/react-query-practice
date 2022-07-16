7b271e0347f29153a8cf330c6c927724f6684b81
Ubeom Kim <kimub1204@gmail.com>
2022-07-16 15:16:37 +0900

    feat: devtools custom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools
+        initialIsOpen={false}
+        panelProps={{
+          className: 'dev-tools',
+          style: {
+            backgroundColor: '#3e87f8',
+          },
+          onClick() {
+            console.log('Click!');
+          },
+        }}
+        position='bottom-right'
+      />
       <div>App</div>
     </QueryClientProvider>
   );
